8bd98e2ff04dd01653817a4608598d98845a2c2b
Bolu Oladipo <boluemmanuel071@gmail.com>
2025-10-04 12:49:52 +0100

    Update chatbotRoutes.js

--- backend/routes/chatbotRoutes.js
+++ backend/routes/chatbotRoutes.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const router = express.Router();
 
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "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";
 
 router.post("/", async (req, res) => {
   try {
